f014958b459d6a68f86c55556b739c3b91673081
pstcl <40257916+pstcl@users.noreply.github.com>
2019-08-21 14:43:27 +0530

    sdafkhads

--- storeapi/src/main/java/org/pstcl/storeapi/repository/SREntityRepository.java
+++ storeapi/src/main/java/org/pstcl/storeapi/repository/SREntityRepository.java
@@ -4,7 +4,7 @@ import org.pstcl.storeapi.model.entity.SREntity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel="srlist",path="srlist")
+//@RepositoryRestResource(collectionResourceRel="srlist",path="srlist")
 public interface SREntityRepository extends PagingAndSortingRepository<SREntity, Integer>{
 
 	
